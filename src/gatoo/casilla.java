3cfcbe7cacc6302513ca62264146fc622b57078d
marianaangeles <37970667+marianaangeles@users.noreply.github.com>
2018-04-04 22:34:43 -0500

    Initial commit

---
+++ src/gatoo/casilla.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gatoo;
+
+import javax.swing.JButton;
+
+/**
+ *
+ * @author mariana
+ */
+public class casilla {
+    JButton A;
+    int B;
+    casilla(){
+        A=new JButton();
+        B=0;
+    }
+}
